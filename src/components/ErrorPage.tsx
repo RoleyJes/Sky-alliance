37d38dd16fe4b98eccee97a2453338f298295874
Jessica <jessicaimoesemi@gmail.com>
2026-01-04 17:50:15 +0100

    error page return button

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,3 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import ModalBtn from "./ModalBtn";
+
 export default function ErrorPage() {
-  return <div className="text-white">Page not found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="h-dvh w-full flex items-center justify-center">
+      <div className="flex flex-col gap-6 text-white">
+        <p className=" text-2xl">Page not found</p>
+        <ModalBtn onClick={() => navigate(-1)}>Go back</ModalBtn>
+      </div>
+    </section>
+  );
 }
